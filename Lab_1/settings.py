03a4fb80b81aa2067bf7d4116902428934d5c497
PsYcHo2601 <sedvlad2015@yandex.ru>
2025-05-25 00:06:02 +0300

    fixed cors error

--- Lab_1/settings.py
+++ Lab_1/settings.py
@@ -26,11 +26,21 @@ SECRET_KEY = 'django-insecure-n@&70+$9jy3^qit0b14f#g(wtik_6iem!)f-=_e5*j0u(5--te
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # фронт
+]
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000"
+]
 
+
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -55,6 +65,8 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
